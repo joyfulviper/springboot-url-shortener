2a94fea6577e5d2b7f02704f3b47705bb30bf870
joyfulviper <kimsk3113@naver.com>
2023-09-30 18:28:39 +0900

    chore : 헬스체크 추가

--- src/main/java/com/prgrms/urlshortener/controller/MainController.java
+++ src/main/java/com/prgrms/urlshortener/controller/MainController.java
@@ -4,6 +4,7 @@ import com.prgrms.urlshortener.utils.URLShortenStrategyConfig;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
@@ -12,5 +13,11 @@ public class MainController {
     public String home(Model model) {
         model.addAttribute("strategies", URLShortenStrategyConfig.StrategyType.values());
         return "home";
+    }
+
+    @GetMapping("/health")
+    @ResponseBody
+    public String health() {
+        return "health";
     }
 }
